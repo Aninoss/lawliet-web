09a5926684ba0737fc6cfe6cdf52792273ce7711
Aninoss <leonblech@tutanota.com>
2021-10-26 19:57:42 +0200

    Fall back to USD on currency retrieval error

--- src/main/java/xyz/lawlietbot/spring/backend/payment/SubCurrency.java
+++ src/main/java/xyz/lawlietbot/spring/backend/payment/SubCurrency.java
@@ -6,12 +6,16 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public enum SubCurrency {
 
     USD('$'),
     EUR('€'),
     GBP('£');
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SubCurrency.class);
 
     public static HashMap<String, SubCurrency> currencyHashMap = new HashMap<>();
 
@@ -25,26 +29,31 @@ public enum SubCurrency {
         return symbol;
     }
 
-    public static synchronized SubCurrency retrieveDefaultCurrency(String ipAddress) throws IOException {
+    public static synchronized SubCurrency retrieveDefaultCurrency(String ipAddress) {
         if (currencyHashMap.containsKey(ipAddress)) {
             return currencyHashMap.get(ipAddress);
         }
 
-        URL ipapi = new URL("http://ipapi.co/" + ipAddress + "/currency/");
-        URLConnection c = ipapi.openConnection();
-        c.setRequestProperty("User-Agent", "java-ipapi-client");
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(c.getInputStream())
-        );
-        String currency = reader.readLine();
-        reader.close();
+        try {
+            URL ipapi = new URL("http://ipapi.co/" + ipAddress + "/currency/");
+            URLConnection c = ipapi.openConnection();
+            c.setRequestProperty("User-Agent", "java-ipapi-client");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(c.getInputStream())
+            );
+            String currency = reader.readLine();
+            reader.close();
 
-        for (SubCurrency value : SubCurrency.values()) {
-            if (value.name().equals(currency)) {
-                currencyHashMap.put(ipAddress, value);
-                return value;
+            for (SubCurrency value : SubCurrency.values()) {
+                if (value.name().equals(currency)) {
+                    currencyHashMap.put(ipAddress, value);
+                    return value;
+                }
             }
+        } catch (IOException e) {
+            LOGGER.error("Error on currency retrieval", e);
         }
+
         currencyHashMap.put(ipAddress, USD);
         return USD;
     }
