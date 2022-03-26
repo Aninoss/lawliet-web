b27895cf59617b748b5b8f52f7eed6d4377fdaf0
Aninoss <leonblech@tutanota.com>
2022-03-26 13:40:32 +0100

    DashboardGrid update

--- src/main/java/xyz/lawlietbot/spring/frontend/components/dashboard/adapters/DashboardGridAdapter.java
+++ src/main/java/xyz/lawlietbot/spring/frontend/components/dashboard/adapters/DashboardGridAdapter.java
@@ -2,10 +2,8 @@ package xyz.lawlietbot.spring.frontend.components.dashboard.adapters;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import dashboard.component.DashboardGrid;
 import dashboard.data.GridRow;
@@ -15,9 +13,6 @@ public class DashboardGridAdapter extends FlexLayout {
     public DashboardGridAdapter(DashboardGrid dashboardGrid) {
         setFlexDirection(FlexDirection.COLUMN);
         add(generateGrid(dashboardGrid));
-        if (dashboardGrid.isWithAddButton()) {
-            add(generateAddButton(dashboardGrid));
-        }
     }
 
     private Component generateGrid(DashboardGrid dashboardGrid) {
@@ -33,29 +28,20 @@ public class DashboardGridAdapter extends FlexLayout {
                     .setHeader(header[i])
                     .setAutoWidth(true);
         }
-        if (dashboardGrid.isWithEditButton()) {
-            grid.addComponentColumn(gridRow -> generateEditButton(dashboardGrid, gridRow))
+        String rowButton = dashboardGrid.getRowButton();
+        if (rowButton.length() > 0) {
+            grid.addComponentColumn(gridRow -> generateRowButton(dashboardGrid, gridRow, rowButton))
                     .setTextAlign(ColumnTextAlign.END)
                     .setWidth("50px");
         }
         return grid;
     }
 
-    private Component generateEditButton(DashboardGrid dashboardGrid, GridRow gridRow) {
-        Button editButton = new Button(VaadinIcon.PENCIL.create());
-        editButton.setEnabled(dashboardGrid.isEnabled());
-        editButton.addClickListener(e -> dashboardGrid.triggerEdit(gridRow.getId()));
-        return editButton;
-    }
-
-    private Component generateAddButton(DashboardGrid dashboardGrid) {
-        Button addButton = new Button(getTranslation("dash.grid.add"), VaadinIcon.PLUS.create());
-        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        addButton.setEnabled(dashboardGrid.isEnabled());
-        addButton.setWidthFull();
-        addButton.addClickListener(e -> dashboardGrid.triggerAdd());
-        addButton.getStyle().set("margin-top", "16px");
-        return addButton;
+    private Component generateRowButton(DashboardGrid dashboardGrid, GridRow gridRow, String buttonText) {
+        Button rowButton = new Button(buttonText);
+        rowButton.setEnabled(dashboardGrid.isEnabled());
+        rowButton.addClickListener(e -> dashboardGrid.triggerRow(gridRow.getId()));
+        return rowButton;
     }
 
 }
