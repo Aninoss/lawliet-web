d83c1cd921b4b1333e03c0a355dade8a5fd2573f
Aninoss <leonblech@tutanota.com>
2022-11-12 10:41:20 +0100

    Local font hosting

--- src/main/java/xyz/lawlietbot/spring/frontend/components/LawlietBotLogo.java
+++ src/main/java/xyz/lawlietbot/spring/frontend/components/LawlietBotLogo.java
@@ -1,14 +1,11 @@
 package xyz.lawlietbot.spring.frontend.components;
 
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.server.VaadinServletService;
 import com.vaadin.flow.server.VaadinSession;
-import com.vaadin.flow.shared.ui.LoadMode;
 
-@StyleSheet(value = "https://fonts.googleapis.com/css2?family=Oswald&display=swap", loadMode = LoadMode.LAZY)
 public class LawlietBotLogo extends HorizontalLayout {
 
     public LawlietBotLogo() {
@@ -36,7 +33,7 @@ public class LawlietBotLogo extends HorizontalLayout {
     private void addLawlietText() {
         H2 lawlietText = new H2("Lawliet");
         lawlietText.getStyle()
-                .set("font-family","'Oswald', sans-serif")
+                .set("font-family","Oswald")
                 .set("color", "white")
                 .set("margin", "0 4px 0 12px");
         add(lawlietText);
@@ -45,7 +42,7 @@ public class LawlietBotLogo extends HorizontalLayout {
     private void addBotText() {
         H2 lawlietText = new H2("BOT");
         lawlietText.getStyle()
-                .set("font-family","'Oswald', sans-serif")
+                .set("font-family","Oswald")
                 .set("color", "var(--lumo-primary-text-color)")
                 .set("margin", "0");
         add(lawlietText);
